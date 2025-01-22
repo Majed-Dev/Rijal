25e4e13d9170a803790a3d50c806aaa2e5d151b8
MajedDev <majedalrabaie@gmail.com>
2025-01-23 00:31:17 +0300

    FIXED Buildings

---
+++ Rijal/Assets/Scripts/ObjectiveItem.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework.Internal;
+using UnityEngine;
+
+public class ObjectiveItem : MonoBehaviour
+{
+    
+    [SerializeField] GameObject descriptionUI;
+    private bool isDecriped = false;
+
+    private void Update()
+    {
+
+    }
+    public void Descripe()
+    {
+        
+    }   
+}
